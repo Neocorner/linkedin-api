d71a366cd5a243b0d35cd3d22cf3fcea6d69b387
Neocorner <vladimir@neocorner.fr>
2025-06-26 13:59:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,7 @@ def get_linkedin_url():
 
     return jsonify({"url": None})
 
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
